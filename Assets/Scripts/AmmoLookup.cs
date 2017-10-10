39f2920d2f6ff9f9a320b4e39f816043faee2a14
MIKIEJ-DESKTOP\mikiej <mikiej_boy@hotmail.com>
2017-10-10 22:17:55 +0100

    Initial commit

---
+++ Assets/Scripts/AmmoLookup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AmmoLookup {
+
+
+    public static string GetCorrectAmmo(string Weapon, string slot)
+    {
+        string ammo = "";
+
+        switch (Weapon)
+        {
+            case "Catapult": switch(slot)
+                            {
+                             case "Slot1": ammo  = "fire Ball"; break;
+                             case "Slot2": ammo = "Ice Ball"; break;
+                             }
+                             break;
+            case "Crossbow": switch (slot)
+                            {
+                                case "Slot1": ammo = "Fire Bolt"; break;
+                                case "Slot2": ammo = "Ice Bolt"; break;
+                            }
+                            break;
+
+        }
+
+        return ammo;
+
+    }
+
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public GameObject AmmoPanel;
+    private bool onScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonPressed()
+    {
+        Debug.Log("Button Pressed");
+
+        if (!onScreen)
+        {
+            AmmoPanel.SetActive(true);
+            onScreen = true;
+        }
+        else
+        {
+            AmmoPanel.SetActive(false);
+            onScreen = false;
+        }
+
+
+    }
+
+
+    public void ChangeAmmo(string ammoSlot)
+    {
+        Debug.Log("You chose : " + ammoSlot);
+        Debug.Log(AmmoLookup.GetCorrectAmmo("Crossbow", ammoSlot));
+    }
+}
